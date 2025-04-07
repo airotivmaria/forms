c298ced0cb850f33e5e414bf91d735a354da9c91
Pedro Henrique <pedrohrezendeb07@gmail.com>
2025-04-07 12:43:35 -0300

    ajustando rotas para deploy

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
+const { MongoClient } = require('mongodb');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const uri = process.env.DATABASE_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors());
-app.use(bodyParser.json());
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Conectado ao MongoDB!");
+    } catch (error) {
+        console.error("Erro ao conectar ao MongoDB:", error);
+    }
+}
 
-mongoose.connect('mongodb://localhost:27017/meu-banco', {
-}).then(() => {
-    console.log('Conectado ao MongoDB');
-}).catch(err => {
-    console.error('Erro ao conectar ao MongoDB', err);
-});
+connect();
 
-app.use('/api/users', userRoutes);
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
